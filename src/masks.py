7742e29d66aa6135947d93881b769ec33d23b165
Vladislav <parovatkinvlad@gmail.com>
2025-08-23 17:19:53 +0300

    Убрал print при вызове функци и сохранил замаскированные значения в переменные

--- src/masks.py
+++ src/masks.py
@@ -16,5 +16,5 @@ def get_mask_account(account_number: int) -> str:
         return "Номер счета неверный"
 
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+get_mask_card_number(7000792289606361)
+get_mask_account(73654108430135874305)
